fd2a803f9be0f44075fc7d3907fb6c38fb957f8a
Daishikofy <daishi.setsuko@gmail.com>
2019-11-23 19:52:05 -0300

    improve loading

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,31 +8,34 @@ using UnityEngine.SceneManagement;
 {
     [SerializeField]
     private Player player;
-    void Awake()
+
+    private int loadedLevelBuildIndex = 0;
+    void Start()
     {
-        /*if (Application.isEditor)
-        {*/
-            Scene loadedLevel = SceneManager.GetSceneByName("Level Forest1");
-            if (loadedLevel.isLoaded)
-            {
-                SceneManager.SetActiveScene(loadedLevel);
-                GetComponent<DynamicGrid>().loadObstacles();
-                return;
-            }
-        //}
-        StartCoroutine(LoadLevel());
+        int index = 2;
+        Scene loadedLevel = SceneManager.GetSceneByBuildIndex(index);
+        if (loadedLevel.isLoaded)
+        {
+            SceneManager.SetActiveScene(loadedLevel);
+            GetComponent<DynamicGrid>().loadObstacles();
+            return;
+        }
+        else
+            StartCoroutine(LoadLevel(index));
     }
 
-    IEnumerator LoadLevel()
+    IEnumerator LoadLevel(int levelBuildIndex)
     {
         enabled = false;
-        yield return SceneManager.LoadSceneAsync(
-            "Level Forest1", LoadSceneMode.Additive
-        );
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Level Forest1"));
+        if (loadedLevelBuildIndex > 0 && loadedLevelBuildIndex != levelBuildIndex)
+            yield return SceneManager.UnloadSceneAsync(loadedLevelBuildIndex);
+
+        yield return SceneManager.LoadSceneAsync(levelBuildIndex, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(levelBuildIndex));
+        loadedLevelBuildIndex = levelBuildIndex;
         enabled = true;
         GetComponent<DynamicGrid>().loadObstacles();
         //DEBUG: Hard coded, need to create a dedicated script later
-        GetComponent<DynamicGrid>().placeInGrid(Vector2.zero,player.gameObject);
+        //GetComponent<DynamicGrid>().placeInGrid(Vector2.zero,player.gameObject);
     }
 }
