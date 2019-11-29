be1d54fda9dbc1f2364b4e72bcb101bd9e00582e
Daishikofy <daishi.setsuko@gmail.com>
2019-11-28 22:59:10 -0300

    Added spikes that hurts the player

---
+++ Assets/Scripts/TriggeredObjects/spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class spikes : MonoBehaviour
+{
+    [SerializeField]
+    int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<DynamicGrid>().placeInGrid(transform.position, this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+            player.OnDamaged(damage, Element.None);
+    }
+}
